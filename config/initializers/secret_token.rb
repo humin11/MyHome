b480c68bd45bec23138c56849672a0f469866ce6
minispring <minispring1013@gmail.com>
2012-09-01 23:42:20 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyHome::Application.config.secret_token = '2b6f364723c197be3e37fbdb01ed38b5f874abff846d73fd82159266552bece0dd016a6b01b77b53db905ad2d4825572ca8def57a061181e5d0372c521780c65'
